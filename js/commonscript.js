7521656e0b411489a71dc0579091684f348f45de
HOWWAY <hwsun.design@gmail.com>
2018-01-04 23:47:24 -0500

    Add files via upload

--- js/commonscript.js
+++ js/commonscript.js
@@ -1,5 +1,6 @@
 $(function () {
             $(".likeheart").click(function () {
+                $(this).parent().toggleClass('like-papa').toggleClass('text-muted');
                 $(this).toggleClass('like-click');
             })
 })
